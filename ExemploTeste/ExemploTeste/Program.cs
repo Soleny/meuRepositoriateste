baf350b88e0865e78c694ce573377bc808ad5d16
Soleny Abadia Rodrigues de Assis <soleny.assis@prodest.es.gov.br.local>
2016-08-11 09:09:08 -0300

    Fiz um exemplo de teste

---
+++ ExemploTeste/ExemploTeste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploTeste
+{
+    class Calculadora
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int soma (int a, int b)
+        {
+
+            return a + b;
+        }
+
+        }
+    }
+
